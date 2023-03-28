911ecc87d7145a64d64cf93de183796586ae5214
hajri <naoures.hajri1@esprit.tn>
2023-03-28 14:14:49 +0200

    jsonFile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const multer = require('multer')
 
 const fs = require('fs')
 const pdfparser = require('pdf-parse')
-const {PDFNet} = require('@pdftron/pdfnet-node');
+var https = require("https");
  
 const path = require('path')
  
@@ -48,41 +48,314 @@ pdfparser(pdfFile).then(function(data){
 
 
 //modifier 
-app.get('/generateInvoice',(req,res)=>{
-  const inputPath = path.resolve(__dirname,'./uploads/1679308997054.pdf');
-  const ouputPath = path.resolve(__dirname,'./uploads/cvCorrect.pdf');
 
-  const replaceText = async() =>{
-    const pdfDoc = await PDFNet.PDFDoc.createFromFilePath(inputPath);
-    await pdfDoc.initStdSecurityHandler();
-    const replacer = await PDFNet.ContentReplacer.create();
-    await replacer.addString('NAOURES HAJRI','HAJRI NAOURES');
-    pdfDoc.save(ouputPath,PDFNet.SDFDoc.SaveOptions.e_linearized);
-  }
-   // add your own license key as the second parameter, e.g. in place of 'YOUR_LICENSE_KEY'.
-PDFNet.runWithCleanup(replaceText,"demo:1679655292865:7d12d2c10300000000afbe89e4384bcf73302013b1089b4c628ea71f2c").then(()=>{
-  fs.readFile(ouputPath,(err,data)=>
-  {if(err){
-    res.statusCode=500;
-    res.end(err);
-  }else{
-    res.setHeader('ContentType','application/pdf');
-    res.end(data);
-  }})
-})  .catch(err => {
-  res.statusCode=500;
-  res.end(err);
+// `request` module is required for file upload.
+// Use "npm install request" command to install.
+var request = require("request");
+
+// The authentication key (API Key).
+// Get your own by registering at https://app.pdf.co
+const API_KEY = "naoures.hajri91@gmail.com_3b402f38178a9d851e4a4270bd178146c995efecbf4d36b08dcf11e82af27cb71fd2d7cd";
+
+
+// Source PDF file
+const SourceFile = "./uploads/1679308997054.pdf";
+// PDF document password. Leave empty for unprotected documents.
+const Password = "";
+// Destination PDF file name
+const DestinationFile = "./uploads/jsonFile.json";
+const Pages = "";
+
+/* // 1. RETRIEVE PRESIGNED URL TO UPLOAD FILE.
+getPresignedUrl(API_KEY, SourceFile)
+    .then(([uploadUrl, uploadedFileUrl]) => {
+        // 2. UPLOAD THE FILE TO CLOUD.
+        uploadFile(API_KEY, SourceFile, uploadUrl)
+            .then(() => {
+                // 3. Replace Text FROM UPLOADED PDF FILE
+                replaceStringFromPdf(API_KEY, uploadedFileUrl, Password, DestinationFile);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    })
+    .catch(e => {
+        console.log(e);
+    });
+
+
+function getPresignedUrl(apiKey, localFile) {
+    return new Promise(resolve => {
+        // Prepare request to `Get Presigned URL` API endpoint
+        let queryPath = `/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name=${path.basename(SourceFile)}`;
+        let reqOptions = {
+            host: "api.pdf.co",
+            path: encodeURI(queryPath),
+            headers: { "x-api-key": API_KEY }
+        };
+        // Send request
+        https.get(reqOptions, (response) => {
+            response.on("data", (d) => {
+                let data = JSON.parse(d);
+                if (data.error == false) {
+                    // Return presigned url we received
+                    resolve([data.presignedUrl, data.url]);
+                }
+                else {
+                    // Service reported error
+                    console.log("getPresignedUrl(): " + data.message);
+                }
+            });
+        })
+            .on("error", (e) => {
+                // Request error
+                console.log("getPresignedUrl(): " + e);
+            });
+    });
 }
-) })
 
- 
- 
+function uploadFile(apiKey, localFile, uploadUrl) {
+    return new Promise(resolve => {
+        fs.readFile(SourceFile, (err, data) => {
+            request({
+                method: "PUT",
+                url: uploadUrl,
+                body: data,
+                headers: {
+                    "Content-Type": "application/octet-stream"
+                }
+            }, (err, res, body) => {
+                if (!err) {
+                    resolve();
+                }
+                else {
+                    console.log("uploadFile() request error: " + e);
+                }
+            });
+        });
+    });
+}
+
+function replaceStringFromPdf(apiKey, uploadedFileUrl, password, destinationFile) {
+    // Prepare request to `Replace Text from PDF` API endpoint
+    var queryPath = `/v1/pdf/edit/replace-text`;
+
+    // JSON payload for api request
+    var jsonPayload = JSON.stringify({
+        name: path.basename(destinationFile), password: password, url: uploadedFileUrl, async: true, searchString: 'NAOURES HAJRI', replaceString: 'HAJRI NAOURES'
+    });
+
+    var reqOptions = {
+        host: "api.pdf.co",
+        method: "POST",
+        path: queryPath,
+        headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+        }
+    };
+    // Send request
+    var postRequest = https.request(reqOptions, (response) => {
+        response.on("data", (d) => {
+            response.setEncoding("utf8");
+
+            // Parse JSON response
+            let data = JSON.parse(d);
+            console.log(`Job #${data.jobId} has been created!`);
+
+            if (data.error == false) {
+                checkIfJobIsCompleted(data.jobId, data.url, destinationFile);
+            }
+            else {
+                // Service reported error
+                console.log("readBarcodes(): " + data.message);
+            }
+        });
+    })
+        .on("error", (e) => {
+            // Request error
+            console.log("readBarcodes(): " + e);
+        });
+
+    // Write request data
+    postRequest.write(jsonPayload);
+    postRequest.end();
+}
+
+function checkIfJobIsCompleted(jobId, resultFileUrl, destinationFile) {
+    let queryPath = `/v1/job/check`;
+
+    // JSON payload for api request
+    let jsonPayload = JSON.stringify({
+        jobid: jobId
+    });
+
+    let reqOptions = {
+        host: "api.pdf.co",
+        path: queryPath,
+        method: "POST",
+        headers: {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+        }
+    };
+
+    // Send request
+    var postRequest = https.request(reqOptions, (response) => {
+        response.on("data", (d) => {
+            response.setEncoding("utf8");
+
+            // Parse JSON response
+            let data = JSON.parse(d);
+            console.log(`Checking Job #${jobId}, Status: ${data.status}, Time: ${new Date().toLocaleString()}`);
+
+            if (data.status == "working") {
+                // Check again after 3 seconds
+                setTimeout(function () { checkIfJobIsCompleted(jobId, resultFileUrl, destinationFile); }, 3000);
+            }
+            else if (data.status == "success") {
+                // Download PDF file
+                var file = fs.createWriteStream(destinationFile);
+                https.get(resultFileUrl, (response2) => {
+                    response2.pipe(file)
+                        .on("close", () => {
+                            console.log(`Generated PDF file saved as "${destinationFile}" file.`);
+                        });
+                });
+            }
+            else {
+                console.log(`Operation ended with status: "${data.status}".`);
+            }
+        })
+    });
+
+    // Write request data
+    postRequest.write(jsonPayload);
+    postRequest.end();
+}
+ */
+// 1. RETRIEVE PRESIGNED URL TO UPLOAD FILE.
+getPresignedUrl(API_KEY, SourceFile)
+    .then(([uploadUrl, uploadedFileUrl]) => {
+        // 2. UPLOAD THE FILE TO CLOUD.
+        uploadFile(API_KEY, SourceFile, uploadUrl)
+            .then(() => {
+                // 3. CONVERT UPLOADED PDF FILE TO JSON
+                convertPdfToJson(API_KEY, uploadedFileUrl, Password, Pages, DestinationFile);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    })
+    .catch(e => {
+        console.log(e);
+    });
 
 
+function getPresignedUrl(apiKey, localFile) {
+    return new Promise(resolve => {
+        // Prepare request to `Get Presigned URL` API endpoint
+        let queryPath = `/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name=${path.basename(SourceFile)}`;
+        let reqOptions = {
+            host: "api.pdf.co",
+            path: encodeURI(queryPath),
+            headers: { "x-api-key": API_KEY }
+        };
+        // Send request
+        https.get(reqOptions, (response) => {
+            response.on("data", (d) => {
+                let data = JSON.parse(d);
+                if (data.error == false) {
+                    // Return presigned url we received
+                    resolve([data.presignedUrl, data.url]);
+                }
+                else {
+                    // Service reported error
+                    console.log("getPresignedUrl(): " + data.message);
+                }
+            });
+        })
+            .on("error", (e) => {
+                // Request error
+                console.log("getPresignedUrl(): " + e);
+            });
+    });
+}
 
+function uploadFile(apiKey, localFile, uploadUrl) {
+    return new Promise(resolve => {
+        fs.readFile(SourceFile, (err, data) => {
+            request({
+                method: "PUT",
+                url: uploadUrl,
+                body: data,
+                headers: {
+                    "Content-Type": "application/octet-stream"
+                }
+            }, (err, res, body) => {
+                if (!err) {
+                    resolve();
+                }
+                else {
+                    console.log("uploadFile() request error: " + e);
+                }
+            });
+        });
+    });
+}
 
+function convertPdfToJson(apiKey, uploadedFileUrl, password, pages, destinationFile) {
+    // Prepare request to `PDF To JSON` API endpoint
+    var queryPath = `/v1/pdf/convert/to/json`;
 
+    // JSON payload for api request
+    var jsonPayload = JSON.stringify({
+        name: path.basename(destinationFile), password: password, pages: pages, url: uploadedFileUrl
+    });
 
+    var reqOptions = {
+        host: "api.pdf.co",
+        method: "POST",
+        path: queryPath,
+        headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+        }
+    };
+    // Send request
+    var postRequest = https.request(reqOptions, (response) => {
+        response.on("data", (d) => {
+            response.setEncoding("utf8");
+            // Parse JSON response
+            let data = JSON.parse(d);
+            if (data.error == false) {
+                // Download JSON file
+                var file = fs.createWriteStream(destinationFile);
+                https.get(data.url, (response2) => {
+                    response2.pipe(file)
+                        .on("close", () => {
+                            console.log(`Generated JSON file saved as "${destinationFile}" file.`);
+                        });
+                });
+            }
+            else {
+                // Service reported error
+                console.log("convertPdfToJson(): " + data.message);
+            }
+        });
+    })
+        .on("error", (e) => {
+            // Request error
+            console.log("convertPdfToJson(): " + e);
+        });
+
+    // Write request data
+    postRequest.write(jsonPayload);
+    postRequest.end();
+}
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
