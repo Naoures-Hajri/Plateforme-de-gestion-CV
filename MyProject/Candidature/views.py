9c04d4cbb63e14a35028193bf9b2c07ca429a104
hajri <naoures.hajri1@esprit.tn>
2023-05-13 14:36:49 +0200

    upload

--- MyProject/Candidature/views.py
+++ MyProject/Candidature/views.py
@@ -7,14 +7,59 @@ import re
 import json
 import collections
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+import os
+@api_view(['POST'])
+def file_upload(request):
+    destination_path = 'MyProject/uploads'
+    uploaded_file = request.FILES['file']
+    with open(os.path.join(destination_path, uploaded_file.name), 'wb+') as destination_file:
+            for chunk in uploaded_file.chunks():
+                destination_file.write(chunk)
+    file_name = os.path.join(destination_path, uploaded_file.name)
+    doc = PyPDF2.PdfFileReader(file_name)
 
+    pages = doc.getNumPages()
+    text = ""
+    for i in range(pages):
+            current_page = doc.getPage(i)
+            text += current_page.extractText()
 
+    text_split = [i.strip() for i in text.split('\n')]
+    # sections_in_resume = [i for i in text_split if i.lower() in sections]
+    entities = {}
+    key = False
+    for phrase in text_split:
+            if len(phrase) == 1:
+                p_key = phrase
+            else:
+                p_key = set(phrase.lower().split()) & set(cs.RESUME_SECTIONS)
+            try:
+                p_key = list(p_key)[0]
+            except IndexError:
+                pass
+            if p_key in cs.RESUME_SECTIONS:
+                entities[p_key] = []
+                key = p_key
+            elif key and phrase.strip():
+                entities[key].append(phrase)
+
+    
+
+    print(entities)
+    return JsonResponse(entities)            
+   
 # Create your views here.
 class MyDataView(APIView):
-    def get(self, request, format=None):
-       
+    def post(request):
         
-        file_name = "MyProject/cv1.pdf"
+        destination_path = 'MyProject/uploads'
+        uploaded_file = request.FILES['file']
+        with open(os.path.join(destination_path, uploaded_file.name), 'wb+') as destination_file:
+            for chunk in uploaded_file.chunks():
+                destination_file.write(chunk)
+               
+        file_name = uploaded_file.name
         doc = PyPDF2.PdfFileReader(file_name)
 
         pages = doc.getNumPages()
@@ -44,6 +89,6 @@ class MyDataView(APIView):
 
     
 
-        
+      
         return JsonResponse(entities)
     
