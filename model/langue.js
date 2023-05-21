a1afdc239e9e9f50bafb5d043fa8bf2d324c483c
hajri <naoures.hajri1@esprit.tn>
2023-05-21 06:07:15 +0200

    modified

--- model/langue.js
+++ model/langue.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const Langue = mongoose.model(
     'Langue',
-    new mongoose.Schema([{
-        titre : {type:String},
+    new mongoose.Schema({
+        titre :[ {type:String}],
             
-        niveau : {type:String},
+        niveau : [{type:String}],
             
         
-            } ]
+            } 
     )
 );
 module.exports=Langue;
